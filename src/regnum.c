27872a9a26be9fb7e4c47fb05313858e88bc0b50
Kostiantyn Shyrolapov <kostya.shirolapov@gmail.com>
2023-12-30 01:05:15 -0500

    rectangle

--- src/regnum.c
+++ src/regnum.c
@@ -58,21 +58,42 @@ void regnum_entrypoint(void (*main_loop)(void), void (*input_loop)(void)) {
     glDeleteShader(vertex_shader);
     glDeleteShader(fragmentShader);
 
-    float vertices[] = {-0.5f, -0.5f, 0.0f, 0.5f, -0.5f,
-                        0.0f,  0.0f,  0.5f, 0.0f};
-    unsigned int VBO, VAO;
+    float vertices[] = {
+        0.5f,  0.5f,  0.0f, // top right
+        0.5f,  -0.5f, 0.0f, // bottom right
+        -0.5f, -0.5f, 0.0f, // bottom left
+        -0.5f, 0.5f,  0.0f  // top left
+    };
+    unsigned int indices[] = {
+        // note that we start from 0!
+        0, 1, 3, // first triangle
+        1, 2, 3  // second triangle
+    };
+
+    unsigned int VBO, VAO, EBO;
 
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
+
     glBindVertexArray(VAO);
+
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices,
+                 GL_STATIC_DRAW);
+
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float),
                           (void *)0);
     glEnableVertexAttribArray(0);
 
     glBindBuffer(GL_ARRAY_BUFFER, 0);
     glBindVertexArray(0);
+
+    // wifeframe mode
+    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
     while (!glfwWindowShouldClose(window)) {
         input_loop();
@@ -82,7 +103,7 @@ void regnum_entrypoint(void (*main_loop)(void), void (*input_loop)(void)) {
         glClear(GL_COLOR_BUFFER_BIT);
         glUseProgram(shaderProgram);
         glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
         main_loop();
 
@@ -90,6 +111,10 @@ void regnum_entrypoint(void (*main_loop)(void), void (*input_loop)(void)) {
         glfwPollEvents();
     }
 
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    glDeleteBuffers(1, &EBO);
+    glDeleteProgram(shaderProgram);
     free(vertex_shader_source);
     free(fragment_shader_source);
     glfwTerminate();
--- src/shaders/fragment_shader.frag
+++ src/shaders/fragment_shader.frag
@@ -1,15 +1,7 @@
 #version 330 core
-in vec3 ourColor;
-in vec2 TexCoord;
-
-out vec4 color;
-
-// Texture samplers
-uniform sampler2D ourTexture1;
-uniform sampler2D ourTexture2;
+out vec4 FragColor;
 
 void main()
 {
-    // Linearly interpolate between both textures (second texture is only slightly combined)
-    color = mix(texture(ourTexture1, TexCoord), texture(ourTexture2, TexCoord), 0.2);
-}
+    FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);
+} 
